702837e44b402a38f2093a293062cc83b7440ee0
calvin.zhu <czhu@redhat.com>
2017-11-03 13:45:27 -0400

    test

--- SecureServiceRecorder/src/main/java/com/redhat/syseng/openshift/service/persistent/model/ProvisionRecord.java
+++ SecureServiceRecorder/src/main/java/com/redhat/syseng/openshift/service/persistent/model/ProvisionRecord.java
@@ -6,11 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
-@NamedQuery(name = "Product.findFeatured", query = "SELECT p FROM Product p WHERE p.featured = true")
 @XmlRootElement
 public class ProvisionRecord
 {
