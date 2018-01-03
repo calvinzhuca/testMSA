71612145bee32dff0ad379c091d4f19424f182ab
calvin.zhu <czhu@redhat.com>
2018-01-03 15:40:59 -0500

    test

--- SecureServiceBroker/src/main/resources/ReadIdHelper.groovy
+++ SecureServiceBroker/src/main/resources/ReadIdHelper.groovy
@@ -1,18 +1,5 @@
 package utilities
 
 class ReadIdHelper {
-    static String say() {
-        // Hello World
-        return "Hello world!"
-    }
-    
-    static String getServiceId(def inputString) {
-        String serviceId = ""
-        if (null != inputString && "" != inputString){
-            if (inputString.indexOf("<service_id>") > -1){
-                serviceId = inputString.substring(inputString.indexOf("<service_id>") + "<service_id>".length(), inputString.indexOf("</service_id>"));
-            }
-        }
-        return serviceId
-    }    
+   
 }
