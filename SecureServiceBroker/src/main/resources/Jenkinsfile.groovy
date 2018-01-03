1a21d74d1ba08b4a99acbbff15cefd8f23207e74
calvin.zhu <czhu@redhat.com>
2018-01-03 16:33:44 -0500

    test

--- SecureServiceBroker/src/main/resources/Jenkinsfile.groovy
+++ SecureServiceBroker/src/main/resources/Jenkinsfile.groovy
@@ -2,23 +2,19 @@
 
 node { 
 
-    /*
+    
     stage ('create service') {
-    echo 'Hello World2' 
-    def username = 'Jenkins'
-    echo 'Hello Mr. ${username}'
-    echo "I said, Hello Mr. ${username}"    
 
     def accessToken = "55044249b6efeaa6ff383df3ac3709824ba51f79438ef5aa57b134e381120c78"
         
-    def serviceName = "test73"
+    def serviceName = "test74"
     def ampURL = "\"https://3scale-admin.middleware.ocp.cloud.lab.eng.bos.redhat.com/admin/api/services.xml\""
     def createServiceCurl = "curl -v -k -X POST -d \"access_token=" + accessToken + "&name=" + serviceName + "\" " + ampURL + " >out.txt"
     echo " createServiceCurl: ${createServiceCurl}"
         
     sh "${createServiceCurl}"
     }
-     */
+    
 
     stage ('create application plan'){
         // Git checkout before load source the file
@@ -26,7 +22,6 @@ node {
 
         // To know files are checked out or not
         sh "ls -lhrt"
-
         def rootDir = pwd()
         println("Current Directory: " + rootDir)
 
@@ -40,8 +35,8 @@ node {
         //externalMethod.lookAtThis("Steve")
         
         
-        def ReadIdHelper = load("ReadIdHelper.groovy")
         echo 'Here123456' 
+        def ReadIdHelper = load("ReadIdHelper.groovy")
         def returnStr = ReadIdHelper.getServiceId(createServiceReply)
         echo "serviceId ${returnStr}"
         //def serviceId = ReadIdHelper.getServiceId(createServiceReply)
